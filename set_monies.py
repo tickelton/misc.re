3ae106531e984c7c49836daf83639020dded4983
tickelton <tickelton@gmail.com>
2020-08-29 17:09:32 +0200

    set_monies.py working on test data

--- set_monies.py
+++ set_monies.py
@@ -15,35 +15,32 @@ def int_to_scan_string(num):
 def str_to_scan_string(in_str):
     return ' '.join(hex(ord(c))[2:] for c in in_str)
 
-if len(sys.argv) != 4:
-    print("Usage: set_monies.py OLD_MONIES OLD_RANK NAME")
+if len(sys.argv) != 6:
+    print("Usage: set_monies.py OLD_MONIES OLD_RANK NAME NEW_MONIES NEW_RANK")
     sys.exit(1)
 
-print(sys.argv[1])
 monies_str = int_to_scan_string(sys.argv[1])
-print(monies_str)
 rank_str = int_to_scan_string(sys.argv[2])
-print(rank_str)
 name_str = str_to_scan_string(sys.argv[3])
+new_monies = int(sys.argv[4])
+new_rank = int(sys.argv[5])
 
 session = frida.attach("snowrunner.exe")
 script = session.create_script("""
     var ranges = Process.enumerateRanges('rw-');
+    var candidates = [];
 
-    function on_match(address, size) {
-        console.log('[+] Pattern found at: ' + address.toString());
-        //Memory.writeInt(address, 111235);
-    }
-
-    console.log('START');
-
-   for (var i in ranges) {
+    for (var i in ranges) {
         var results = Memory.scanSync(ranges[i].base, ranges[i].size, '%s');
         for (var j in results) {
             // Found old money value in current range, keep looking for 
             // other variables.
             console.log('range=' + JSON.stringify(ranges[i]));
-            console.log('address=' + results[j].address);
+            console.log('money=' + results[j].address);
+            var addr_money = results[j].address;
+            var addr_name = '';
+            var addr_rank = '';
+
 
             var new_search = {
                 'base': '0x' + (parseInt(results[j].address, 16) - 64).toString(16),
@@ -54,20 +51,34 @@ script = session.create_script("""
             var profile_name = Memory.scanSync(ptr(new_search.base), new_search.size, '%s');
             for (var k in profile_name) {
                 console.log('name=' + profile_name[k].address);
+                addr_name = profile_name[k].address;
             }
             var rank = Memory.scanSync(ptr(new_search.base), new_search.size, '%s');
             for (var k in rank) {
                 console.log('rank=' + rank[k].address);
+                addr_rank = rank[k].address;
             }
-            console.log('NEXT');
+            if (addr_name && addr_money && addr_rank) {
+                candidates.push({'name': addr_name, 'rank': addr_rank, 'money': addr_money});
+            }
         }
     }
 
-    console.log('FINISHED');
+    if (candidates.length) {
+        console.log('Found candidates: ' + JSON.stringify(candidates));
+    } else {
+        console.log('Target data not found.');
+    }
 
-""" % (monies_str, name_str, rank_str))
+    if (candidates.length == 1) {
+        console.log('Updating data!');
+        Memory.writeInt(candidates[0].money, %d);
+        Memory.writeInt(candidates[0].rank, %d);
+    } else if (candidates.length > 1) {
+        console.log('Ambiguous result. Not updating!');
+    }
+
+""" % (monies_str, name_str, rank_str, new_monies, new_rank))
 script.load()
-sys.stdin.read()
 
-
-
+input('Press <Enter> to quit.')
