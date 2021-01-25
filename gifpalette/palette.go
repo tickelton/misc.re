a0c759f1798b43d74c119f5171f3a6f05ba6a5bf
tickelton <tickelton@gmail.com>
2021-01-25 21:55:05 +0100

    Improves output of palette.go
    * Includes more example images.
    * Palettes of all subimages are now printed.
    * Palettes are printed in Go notation.

--- gifpalette/palette.go
+++ gifpalette/palette.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"image/color"
 	"image/gif"
 	"log"
 	"os"
 )
 
 func main() {
-	reader, err := os.Open("/media/ramdisk/rgb.gif")
+	reader, err := os.Open("animated.gif")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -16,5 +17,24 @@ func main() {
 	defer reader.Close()
 
 	g, err := gif.DecodeAll(reader)
-	fmt.Println(g.Image[0].Palette[4])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for i := 0; i < len(g.Image); i++ {
+		fmt.Println("color.Palette{")
+		for j := 0; j < len(g.Image[i].Palette); j++ {
+			entry, ok := g.Image[i].Palette[j].(color.RGBA)
+			if ok {
+				fmt.Printf(
+					"\tcolor.RGBA{%#02x, %#02x, %#02x, %#02x},\n",
+					entry.R,
+					entry.G,
+					entry.B,
+					entry.A,
+				)
+			}
+		}
+		fmt.Println("}")
+	}
 }
